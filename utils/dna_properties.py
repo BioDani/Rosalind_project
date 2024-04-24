f8fc33ea75383ba54fea7276be3b9947f328c35b
BioDani <dtejada@unal.edu.co>
2024-04-24 17:55:41 -0500

    corrección detalles  seq Upper()
    Signed-off-by: BioDani <dtejada@unal.edu.co>

--- utils/dna_properties.py
+++ utils/dna_properties.py
@@ -1,49 +1,54 @@
-def count_A_nucleotide(sequence: str) -> int:
+def count_A_nucleotide(base_seq: str) -> int:
     """Counts the occurrences of nucleotide A in the sequence.
 
     Returns:
         The number of occurrences of 'A'.
     """
+    sequence = base_seq.upper()
     return sequence.count("A")
 
-def count_C_nucleotide(sequence: str) -> int:
+def count_C_nucleotide(base_seq: str) -> int:
     """Counts the occurrences of nucleotide C in the sequence.
 
     Returns:
         The number of occurrences of 'C'.
     """
+    sequence = base_seq.upper()
     return sequence.count("C")
 
-def count_G_nucleotide(sequence: str) -> int:
+def count_G_nucleotide(base_seq: str) -> int:
     """Counts the occurrences of nucleotide G in the sequence.
 
     Returns:
         The number of occurrences of 'G'.
     """
+    sequence = base_seq.upper()
     return sequence.count("G")
 
-def count_T_nucleotide(sequence: str) -> int:
+def count_T_nucleotide(base_seq: str) -> int:
     """Counts the occurrences of nucleotide T in the sequence.
 
     Returns:
         The number of occurrences of 'T'.
     """
+    sequence = base_seq.upper()
     return sequence.count("T")
 
-def calculate_length(sequence: str) -> int:
+def calculate_length(base_seq: str) -> int:
     """Calculates the length of the DNA sequence.
 
     Returns:
         The length of the sequence.
     """
-    return len(sequence)
+    return len(base_seq)
 
-def calculate_gc_content(sequence: str) -> float:
+def calculate_gc_content(base_seq: str) -> float:
     """Calculates the GC content (percentage of G and C) of the sequence.
 
     Returns:
         The GC content as a float percentage (0.0 to 1.0).
     """
+    sequence = base_seq.upper()
     return  (sequence.count("C") + sequence.count("G") ) \
         / calculate_length(sequence)
     
