119f402ac7615ded1cc815dc4855df5cb98c368d
BioDani <dtejada@unal.edu.co>
2024-04-26 17:27:21 -0500

    organización del paquete
    Signed-off-by: BioDani <dtejada@unal.edu.co>

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,20 +1,20 @@
-from dna_properties import count_A_nucleotide
-from dna_properties import count_C_nucleotide
-from dna_properties import count_G_nucleotide
-from dna_properties import count_T_nucleotide
-from dna_properties import calculate_gc_content
-from dna_properties import calculate_length
-from dna_properties import validate_sequence
+from .dna_properties import count_A_nucleotide
+from .dna_properties import count_C_nucleotide
+from .dna_properties import count_G_nucleotide
+from .dna_properties import count_T_nucleotide
+from .dna_properties import calculate_gc_content
+from .dna_properties import calculate_length
+from .dna_properties import validate_sequence
 
-from rna_properties import traslate_nucleotide_protein
+from .rna_properties import traslate_nucleotide_protein
 
-from fasta_format import read_fasta
+from .fasta_format import read_fasta
 
-from dna_patterns import find_motif
+from .dna_patterns import find_motif
 
-from dna_class import DNASequence
+from .dna_class import DNASequence
 
-from aligments import hamming_distance
+from .aligments import hamming_distance
 
 
 
